00abe308a463a77a832a6dea2e1860f9457c630e
Sukriti Sood <55010599+Sukriti-sood@users.noreply.github.com>
2022-03-11 21:02:14 +0530

    Update _footer.php

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,3 +1,3 @@
 <div class="container-fluid bg-dark text-light footer">
-    <p class="text-center py-2 mb-0">Copyright phpForum  2021 | All rights Reserved.</p>
+    <p class="text-center py-2 mb-0">Copyright phpForum  2021 | All rights are Reserved.</p>
 </div>
