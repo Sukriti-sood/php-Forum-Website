b04e87ef38922782aa58f94f19d128df1bb9065b
Sukriti Sood <55010599+Sukriti-sood@users.noreply.github.com>
2022-03-11 21:35:15 +0530

    Update _header.php

--- partials/_header.php
+++ partials/_header.php
@@ -25,7 +25,7 @@ echo ' <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
         </a>
         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
           <li><a class="dropdown-item" href="#">Action</a></li>
-          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li><a class="dropdown-item" href="#">Another Action</a></li>
           <li><hr class="dropdown-divider"></li>
           <li><a class="dropdown-item" href="#">Something else here</a></li>
         </ul>
