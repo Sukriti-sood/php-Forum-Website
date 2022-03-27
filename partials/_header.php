6586e747fa1a77ccb385152549f9e130556cef41
Sukriti Sood <55010599+Sukriti-sood@users.noreply.github.com>
2022-03-27 10:46:59 +0530

    Update _header.php

--- partials/_header.php
+++ partials/_header.php
@@ -11,7 +11,9 @@ echo ' <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+  
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       <li class="nav-item">
         <a class="nav-link active" aria-current="page" href="./">Home</a>
@@ -34,6 +36,7 @@ echo ' <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
         <a class="nav-link " href="./contact.php" tabindex="-1" >Contact</a>
       </li>
     </ul>
+    
     <form class="d-flex">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-success" type="submit">Search</button>
@@ -49,8 +52,7 @@ echo ' <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
       echo '
       <button class="btn btn-outline-success mx-2" type="submit" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
     <button class="btn btn-outline-success" type="submit" data-bs-toggle="modal" data-bs-target="#signupModal">Signup</button>
-    
-      ';
+     ';
     }
   echo '</div>
 </div>
