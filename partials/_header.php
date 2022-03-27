e0985ac91361da9df4bbcb725e4127d84fcfd721
Sukriti Sood <55010599+Sukriti-sood@users.noreply.github.com>
2022-03-27 11:19:13 +0530

    Update _header.php

--- partials/_header.php
+++ partials/_header.php
@@ -7,7 +7,9 @@ if($_SESSION['loggedin']&&$_SESSION['loggedin']==true)
 }
 echo ' <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
 <div class="container-fluid">
+
   <a class="navbar-brand" href="./">phpForum</a>
+ 
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -25,26 +27,37 @@ echo ' <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
       </li>
       
       <li class="nav-item dropdown">
+      
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           Categories
         </a>
+        
         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
           <li><a class="dropdown-item" href="#">Action</a></li>
+          
           <li><a class="dropdown-item" href="#">Another Action</a></li>
+          
           <li><hr class="dropdown-divider"></li>
+          
           <li><a class="dropdown-item" href="#">Something else here</a></li>
         </ul>
+        
       </li>
       
       <li class="nav-item">
+      
         <a class="nav-link " href="./contact.php" tabindex="-1" >Contact</a>
+        
       </li>
       
     </ul>
     
     <form class="d-flex">
+    
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      
       <button class="btn btn-success" type="submit">Search</button>
+      
     </form>';
     if($logedin)
     {
